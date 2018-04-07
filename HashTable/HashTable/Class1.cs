0fe0fbc51299f22d1edd3f9c9b029bba3ff37235
Jpeex <delikit@mail.ru>
2018-04-07 22:27:34 +0500

    HT

---
+++ HashTable/HashTable/Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashTable
+{
+    public class HashTable
+
+    {
+        class KeyValuePair
+        {
+            public object Key { get; set; }
+            public object Value { get; set; }
+        }
+
+        List<List<KeyValuePair>> list;
+
+        public HashTable(int size)
+        {
+            list = new List<List<KeyValuePair>>();
+            for (int i = 0; i < size; i++)
+            {
+                list.Add(new List<KeyValuePair>());
+            }
+        }
+
+        public void PutPair(object key, object value)
+        {
+            var boolshi = GetBucketNumber(key);
+            foreach (var e in list[boolshi])
+            {
+                if (Equals(e.Key, key))
+                {
+                    e.Value = value;
+                    return;
+                }
+            }
+
+            list[boolshi].Add(new KeyValuePair { Key = key, Value = value });
+        }
+
+        public object GetValueByKey(object key)
+        {
+            var boolshi = GetBucketNumber(key);
+            foreach (var e in list[boolshi])
+            {
+                if (Equals(e.Key, key))
+                {
+                    return e.Value;
+                }
+            }
+
+            return null;
+        }
+
+        private int GetBucketNumber(object key)
+        {
+            return Math.Abs(key.GetHashCode()) % list.Count;
+        }
+    }
+}
---
+++ HashTable/UnitTestProject1.HT/HashTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HashTableTests
+{
+
+    [TestClass]
+    public class HashTableTests
+    {
+        [TestMethod]
+        public void ThreeElements()
+        {
+            var wtf = new HashTable.HashTable(3);
+
+            wtf.PutPair("1", "Один");
+            wtf.PutPair("2", "Два");
+            wtf.PutPair("3", "Три");
+
+            Assert.AreEqual(wtf.GetValueByKey("1"), "Один");
+            Assert.AreEqual(wtf.GetValueByKey("2"), "Два");
+            Assert.AreEqual(wtf.GetValueByKey("3"), "Три");
+        }
+
+        [TestMethod]
+        public void TwoElements()
+        {
+            var wtf = new HashTable.HashTable(3);
+
+            wtf.PutPair("1", "Один");
+            wtf.PutPair("1", "Два");
+
+            Assert.AreEqual(wtf.GetValueByKey("1"), "Два");
+        }
+
+        [TestMethod]
+        public void BigElements()
+        {
+            var wtf = new HashTable.HashTable(10000);
+
+            for (int i = 0; i < 10000; i++)
+            {
+                wtf.PutPair(i, i + "Один");
+            }
+
+            Assert.AreEqual(wtf.GetValueByKey(1), "1Один");
+        }
+
+        [TestMethod]
+        public void DifficultTest()
+        {
+            var wtf = new HashTable.HashTable(11000);
+
+            for (int i = 0; i < 10000; i++)
+            {
+                wtf.PutPair(i, i + "Один");
+            }
+
+            for (int i = 10000; i < 11000; i++)
+            {
+                Assert.AreEqual(wtf.GetValueByKey(i), null);
+            }
+        }
+    }
+}
