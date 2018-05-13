07844df99f1cd1196a212ed4dfb2683e126b1355
Jpeex <delikit@mail.ru>
2018-05-13 12:28:51 +0500

    Yeah

---
+++ Telefonchik/WindowsForm/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace WindowsForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightCyan;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(56, 185);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(207, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.LightCyan;
+            this.textBox1.Location = new System.Drawing.Point(56, 78);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.textBox1.Size = new System.Drawing.Size(294, 82);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.LightCyan;
+            this.textBox2.Location = new System.Drawing.Point(492, 78);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(294, 82);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LightCyan;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Location = new System.Drawing.Point(579, 185);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(207, 39);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Найти";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.LightCyan;
+            this.button5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button5.Location = new System.Drawing.Point(56, 246);
+            this.button5.Margin = new System.Windows.Forms.Padding(4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(207, 39);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Удалить";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.LightCyan;
+            this.button6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button6.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button6.Location = new System.Drawing.Point(579, 246);
+            this.button6.Margin = new System.Windows.Forms.Padding(4);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(207, 39);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Выход";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comfortaa", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(154, 42);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Наименование";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Comfortaa", 10F);
+            this.label3.Location = new System.Drawing.Point(604, 42);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Номерок";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleGreen;
+            this.ClientSize = new System.Drawing.Size(844, 380);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Блокнотик Сметри";
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Telefonchik/WindowsForm/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsForm
+{
+    public struct Phone
+    {
+        public string Name;
+        public string Number;
+
+    }
+
+    public partial class Form1 : Form
+    {
+        List<Phone> newPhone = new List<Phone>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if ((textBox1.Text == null) || (textBox2.Text == null))
+            {
+                MessageBox.Show("Ошибка данных. \n" + "Необходимо ввести данные во все поля.", "Справочник", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+            else
+            {
+                string t1 = textBox1.Text;
+                string textik = File.ReadAllText("C:/Users/jurav/Desktop/text1.txt.txt");
+                using (StreamReader read = new StreamReader(("C:/Users/jurav/Desktop/text1.txt.txt")))
+                {
+                    if (textik.Contains(t1))
+                    {
+                        MessageBox.Show("В меня такой же не влезет");
+                    }
+                    else
+                    {
+
+                        Phone x;
+                        x.Name = textBox1.Text;
+                        x.Number = textBox2.Text;
+                        int x1 = search(x.Name); 
+
+
+                        //MessageBox.Show("Контакт сохранен");
+
+                        if (x1 == -1)
+                        {
+                            newPhone.Add(x);
+                            //textBox1.Text = "";
+                            //textBox2.Text = "";
+                            //MessageBox.Show("Записал");
+                            read.Close();
+                            System.IO.StreamWriter write = new System.IO.StreamWriter("C:/Users/jurav/Desktop/text1.txt.txt", true);
+                            write.WriteLine(textBox1.Text);
+                            write.WriteLine(textBox2.Text);
+                            write.Close();
+                            textBox1.Text = "";
+                            textBox2.Text = "";
+                            MessageBox.Show("Записал");
+                        }
+                        else
+                        {
+                            MessageBox.Show("В меня такой же не влезет");
+                        }
+                    }
+                }
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            newPhone.Clear();
+            Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StreamReader readd = new StreamReader("C:/Users/jurav/Desktop/text1.txt.txt");
+            string m;
+            while ((m = readd.ReadLine())!=null)
+            {
+                Phone ss;
+                ss.Name = m;
+                ss.Number = readd.ReadLine();
+                newPhone.Add(ss);
+            }
+            readd.Close(); 
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if ((textBox1.Text == null) || (textBox2.Text == null))
+            {
+                MessageBox.Show("Ошибка данных. \n" + "Необходимо ввести данные во все поля.", "Справочник", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+            else
+            {
+                Phone x;
+                x.Name = textBox1.Text;
+                x.Number = textBox2.Text;
+                int x1 = search(x.Name);
+                if (x1 == -1)
+                {
+                    newPhone.Add(x);
+                    textBox1.Text = "";
+                    textBox2.Text = "";
+                    MessageBox.Show("Контакт внесён");
+
+                }
+                else
+                {
+                    MessageBox.Show("Данный контакт существует");
+                }
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e) 
+        {
+            //textBox1.Text = null;
+            //textBox2.Text = null;
+
+        }
+        int search(string s)
+        {
+            for (int i = 0; i < newPhone.Count; i++)
+            {
+                if (newPhone[i].Name.Equals(s))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        int search2(string d)
+        {
+            for (int i = 0; i < newPhone.Count; i++)
+            {
+                if (newPhone[i].Number.Equals(d))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string s = textBox1.Text;
+            string d = textBox2.Text;
+            int x = search(s);
+            int y = search2(d);
+            if (x != -1 )
+            {
+                textBox2.Text = newPhone[x].Number;
+
+            }
+            else if (y != -1)
+                textBox1.Text = newPhone[y].Name;
+            else
+            MessageBox.Show("Чот походу нет его");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+                    Phone x;
+                    x.Name = textBox1.Text;
+                    x.Number = textBox2.Text;
+                    int x1 = search(x.Name);
+
+                if (x1 != -1)
+                    {
+                        newPhone.RemoveAt(x1);
+                        textBox1.Text = "";
+                        textBox2.Text = "";
+                        MessageBox.Show("Больше ты его никогда не увидишь");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Хотелось бы его 'убрать', но я его не нашел :( ");
+                    }          
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StreamReader readd = new StreamReader("C:/Users/jurav/Desktop/text1.txt.txt");
+            string m;
+            while ((m = readd.ReadLine()) != null)
+            {
+                Phone ss;
+                ss.Name = m;
+                ss.Number = readd.ReadLine();
+                newPhone.Add(ss);
+            }
+            readd.Close();
+        }
+    }
+}
